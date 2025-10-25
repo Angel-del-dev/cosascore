2e8d631f0ff66295ffa816d7864a236cb5cfa934
Angel <angelnovo15@gmail.com>
2025-10-25 12:24:30 +0200

    Added barebones skeleton for Main model

---
+++ src/lib/models/Model.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace cosascore\src\lib\models;
+
+class Model {
+    public static function Create() {
+
+    }
+
+    public static function Delete() {
+
+    }
+
+    public static function Update() {
+
+    }
+
+    public static function Get() {
+        
+    }
+}
